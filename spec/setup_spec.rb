a08e3824177bf388b790158dea1d842b3e8e1efc
Eric Oestrich <eric@oestrich.org>
2012-11-09 09:44:06 -0500

    Create a setup file

--- spec/setup_spec.rb
+++ spec/setup_spec.rb
@@ -17,4 +17,24 @@ describe Hobostove::Setup do
       it { should be_true }
     end
   end
+
+  describe "Setup" do
+    let(:stdout) { StringIO.new }
+    let(:stdin) { StringIO.new("subdomain\nkey\nroom\n") }
+
+    before { FileUtils.mkdir_p(Dir.home(Etc.getlogin)) }
+
+    subject { Hobostove::Setup.new(stdin, stdout) }
+
+    it "should walk through setup" do
+      subject.run
+
+      config = YAML.load(File.read(Hobostove::Configuration.config_file))
+      config.should == {
+        "subdomain" => "subdomain",
+        "token" => "key",
+        "room" => "room"
+      }
+    end
+  end
 end
